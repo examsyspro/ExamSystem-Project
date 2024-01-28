1a84d6fabf8075f0231ecc3710b975cc78044c2d
roeiatar87 <roeiatar87@gmail.com>
2024-01-28 22:52:26 +0200

    hfghhf

--- ExamSystem-Project/Forms/BuildExamForm.cs
+++ ExamSystem-Project/Forms/BuildExamForm.cs
@@ -28,6 +28,9 @@ namespace ExamSystem_Project.Forms
         public RadioButton radioButton;
         public List<TextBox> textBoxesList;
         public List<RadioButton> radioButtonList;
+        public List<Button> deleteButtonList;
+        public List<Label> labelList;
+        public List<Control> ControlsList;   
 
         public BuildExamForm()
         {
@@ -36,20 +39,27 @@ namespace ExamSystem_Project.Forms
             exam = new Exam();
             textBoxesList = new List<TextBox>();
             radioButtonList = new List<RadioButton>();
+            deleteButtonList = new List<Button>();
+            ControlsList = new List<Control>();
+            labelList = new List<Label>();
+
 
         }
 
 
         public void InitializeAll()
         {
-            comboBox_Course_Select.Text = "Select Course";
 
+
+            comboBox_Course_Select.Text = "Select Course";
             comboBox_Course_Select.Items.Insert(0, Course_Enum.C_Sharp);
             comboBox_Course_Select.Items.Insert(1, Course_Enum.JavaSctipt);
             tabControl1.TabPages.Remove(tabPage_step2);
             tabControl1.TabPages.Remove(tabPage_step3);
             dateTimePicker_examDate.MinDate = DateTime.Now;
             dateTimePicker_examDate.MaxDate = DateTime.Now.AddDays(60);
+
+            panel_questions.SendToBack();
         }
 
         private void button_next_Click(object sender, EventArgs e)
@@ -114,124 +124,159 @@ namespace ExamSystem_Project.Forms
 
 
 
-        private void button_AddQuestion_Click(object sender, EventArgs e)
+
+
+
+
+        public void ClearAllControls()
         {
+            for (int i = 0; i < radioButtonList.Count; i++)
+            {
+                panel_questions.Controls.Remove(labelList[i]);
+                panel_questions.Controls.Remove(textBoxesList[i]);
+                panel_questions.Controls.Remove(radioButtonList[i]);
+                panel_questions.Controls.Remove(deleteButtonList[i]);
+            }
+            textBoxCounter = 0;
+            optionNameCounter = 1;
 
+
+           
+            textBox_QuetionContent.Text = string.Empty;
+            textBoxesList = new List<TextBox>();
+            radioButtonList = new List<RadioButton>();
+            deleteButtonList = new List<Button>();
+            labelList = new List<Label>();
+
+            button_addOption.Enabled = textBoxCounter == 0 ? true : false;
         }
-
 
 
         private void button_addOption_Click(object sender, EventArgs e)
         {
-            button_addOption.Enabled = textBoxCounter == 4 ? true : false;
-
-            // Create a unique identifier for the dynamic controls
-            string controlId = Guid.NewGuid().ToString();
-
-            // Create a new Label
-            dynamicLabel = new Label
+            try
             {
-                Font = new Font("Segoe UI", 11, FontStyle.Bold), // Set Font to bold
-                ForeColor = Color.FromArgb(0, 135, 209),
-                Text = "Option " + optionNameCounter + " :",
-                TextAlign = System.Drawing.ContentAlignment.MiddleLeft,
-                Location = new Point(label_question.Left + 60, Convert.ToInt32(GetTopMargin())),
-                AutoSize = true,
-                Tag = controlId // Set a unique identifier as the Tag
-            };
+                button_addOption.Enabled = textBoxCounter == 4 ? true : false;
 
-            // Create a new TextBox
-            dynamicTextBox = new TextBox
-            {
-                Width = 600,
-                Height = 26,
-                BorderStyle = BorderStyle.FixedSingle,
-                Location = new Point(textBox_QuetionContent.Left, dynamicLabel.Top),
-                Name = "textBox_option" + optionNameCounter, // Unique name based on the counter
-                Tag = controlId, // Set the same unique identifier as the Tag
+                // Create a unique identifier for the dynamic controls
+                string controlId = Guid.NewGuid().ToString();
 
-            };
-
-            textBoxesList.Add(dynamicTextBox);
-
-
-            // Create a Delete button (trash icon)
-            Button deleteButton = new Button
-            {
-                Text = "Delete",
-                Width = 107,
-                Font = new Font("Segoe UI", 11, FontStyle.Bold),
-                Name = "button_" + optionNameCounter,
-                ForeColor = Color.White,
-                BackColor = Color.FromArgb(0, 135, 209),
-                Height = 40,
-                AutoSize = true,
-                Location = new Point(dynamicTextBox.Right + 30, dynamicTextBox.Top - 7),// Use Location instead of Margin
-                Tag = controlId // Set the same unique identifier as the Tag
-            };
-
-            radioButton = new RadioButton
-            {
-                Text = "Is Correct :",
-                Name = "radioButton_IsCorrect" + optionNameCounter,
-                AutoSize = true,
-                CheckAlign = System.Drawing.ContentAlignment.MiddleRight,
-                Font = new Font("Segoe UI", 11, FontStyle.Bold),
-                ForeColor = Color.FromArgb(0, 135, 209),
-                Location = new Point(deleteButton.Right, dynamicTextBox.Top - 4),
-                Tag = controlId
-            };
-
-            radioButtonList.Add(radioButton);
-            // Attach a click event handler to the delete button
-            deleteButton.Click += (deleteSender, deleteEventArgs) =>
-            {
-                if (deleteSender is Button btn)
+                // Create a new Label
+                dynamicLabel = new Label
                 {
-                    var arr = btn.Name.Split("_");
-                    if (int.Parse(arr[1]) < textBoxCounter)
+                    Font = new Font("Segoe UI", 11, FontStyle.Bold), // Set Font to bold
+                    ForeColor = Color.FromArgb(0, 135, 209),
+                    Text = "Option " + optionNameCounter + " :",
+                    TextAlign = System.Drawing.ContentAlignment.MiddleLeft,
+                    Location = new Point(label_question.Left + 60, Convert.ToInt32(GetTopMargin())),
+                    AutoSize = true,
+                    Tag = controlId // Set a unique identifier as the Tag
+                };
+
+                labelList.Add(dynamicLabel);
+                // Create a new TextBox
+                dynamicTextBox = new TextBox
+                {
+                    Width = 600,
+                    Height = 26,
+                    BorderStyle = BorderStyle.FixedSingle,
+                    Location = new Point(textBox_QuetionContent.Left, dynamicLabel.Top),
+                    Name = "textBox_option" + optionNameCounter, // Unique name based on the counter
+                    Tag = controlId, // Set the same unique identifier as the Tag
+
+                };
+
+                textBoxesList.Add(dynamicTextBox);
+
+
+                // Create a Delete button (trash icon)
+                Button deleteButton = new Button
+                {
+                    Text = "Delete",
+                    Width = 107,
+                    Font = new Font("Segoe UI", 11, FontStyle.Bold),
+                    Name = "button_" + optionNameCounter,
+                    ForeColor = Color.White,
+                    BackColor = Color.FromArgb(0, 135, 209),
+                    Height = 40,
+                    AutoSize = true,
+                    Location = new Point(dynamicTextBox.Right + 30, dynamicTextBox.Top - 7),// Use Location instead of Margin
+                    Tag = controlId // Set the same unique identifier as the Tag
+                };
+
+                deleteButtonList.Add(deleteButton);
+
+                radioButton = new RadioButton
+                {
+                    Text = "Is Correct :",
+                    Name = "radioButton_IsCorrect" + optionNameCounter,
+                    AutoSize = true,
+                    CheckAlign = System.Drawing.ContentAlignment.MiddleRight,
+                    Font = new Font("Segoe UI", 11, FontStyle.Bold),
+                    ForeColor = Color.FromArgb(0, 135, 209),
+                    Location = new Point(deleteButton.Right, dynamicTextBox.Top - 4),
+                    Tag = controlId
+                };
+
+                radioButtonList.Add(radioButton);
+
+                // Attach a click event handler to the delete button
+                deleteButton.Click += (deleteSender, deleteEventArgs) =>
+                {
+                    if (deleteSender is Button btn)
                     {
-                        MessageBox.Show($"Delete Option {textBoxCounter} first");
-                        return;
+                        var arr = btn.Name.Split("_");
+                        if (int.Parse(arr[1]) < textBoxCounter)
+                        {
+                            MessageBox.Show($"Delete Option {textBoxCounter} first");
+                            return;
+                        }
                     }
-                }
 
+                    button_addOption.Enabled = textBoxCounter < 5;
+                    textBoxCounter--;
+                    optionNameCounter--;
+                    
+
+
+                    // Retrieve the unique identifier associated with the controls
+                    string tag = deleteButton.Tag as string;
+
+                    textBoxesList.RemoveAll(x => x.Tag == tag);
+                    radioButtonList.RemoveAll(x => x.Tag == tag);
+                    
+                    // Find and remove controls with the same unique identifier
+                    var controlsToRemove = panel_questions.Controls
+                        .Cast<Control>()
+                        .Where(control => control.Tag as string == tag)
+                        .ToList();
+                    
+                    foreach (var control in controlsToRemove)
+                    {
+                        panel_questions.Controls.Remove(control);
+                    }
+                    button_addOption.Enabled = textBoxCounter < 5;
+                };
+
+                // Add Label, TextBox, and Delete button to the panel_questions
+                panel_questions.Controls.Add(dynamicLabel);
+                panel_questions.Controls.Add(dynamicTextBox);
+                panel_questions.Controls.Add(deleteButton);
+                panel_questions.Controls.Add(radioButton);
+
+                // Increment the counter for the next TextBox
+
+                textBoxCounter++;
+                optionNameCounter++;
                 button_addOption.Enabled = textBoxCounter < 5;
-                textBoxCounter--;
-                optionNameCounter--;
+            }
+            catch (Exception ex)
+            {
 
+                
+            }
 
-
-                // Retrieve the unique identifier associated with the controls
-                string tag = deleteButton.Tag as string;
-
-                textBoxesList.RemoveAll(x => x.Tag == tag);
-                radioButtonList.RemoveAll(x => x.Tag == tag);
-
-                // Find and remove controls with the same unique identifier
-                var controlsToRemove = panel_questions.Controls
-                    .Cast<Control>()
-                    .Where(control => control.Tag as string == tag)
-                    .ToList();
-
-                foreach (var control in controlsToRemove)
-                {
-                    panel_questions.Controls.Remove(control);
-                }
-                button_addOption.Enabled = textBoxCounter < 5;
-            };
-
-            // Add Label, TextBox, and Delete button to the panel_questions
-            panel_questions.Controls.Add(dynamicLabel);
-            panel_questions.Controls.Add(dynamicTextBox);
-            panel_questions.Controls.Add(deleteButton);
-            panel_questions.Controls.Add(radioButton);
-
-            // Increment the counter for the next TextBox
-
-            textBoxCounter++;
-            optionNameCounter++;
-            button_addOption.Enabled = textBoxCounter < 5;
+           
         }
 
 
@@ -275,25 +320,36 @@ namespace ExamSystem_Project.Forms
 
         private void button_SaveQuestion_Click(object sender, EventArgs e)
         {
-            question = new Question();
-            question.Text = textBox_QuetionContent.Text;
-
-
-            for (int i = 0; i < textBoxesList.Count; i++)
+            try
             {
-                var box = textBoxesList[i];
-                var radioButton = radioButtonList[i];
+                //question = new Question();
+                question.Text = textBox_QuetionContent.Text;
 
-                if (!string.IsNullOrEmpty(box.Text))
+                for (int i = 0; i < textBoxesList.Count; i++)
                 {
-                    optionAns = new OptionAns();
-                    optionAns.QuestionStrId = question.QuestionStrId;
-                    optionAns.OptionText = box.Text;
-                    optionAns.IsCorrect = radioButton.Checked;
-                    question.Options.Add(optionAns);
+                    var box = textBoxesList[i];
+                    var radioButton = radioButtonList[i];
+
+                    if (!string.IsNullOrEmpty(box.Text))
+                    {
+                        optionAns = new OptionAns();
+                        optionAns.QuestionStrId = question.QuestionStrId;
+                        optionAns.OptionText = box.Text;
+                        optionAns.IsCorrect = radioButton.Checked;
+                        question.Options.Add(optionAns);
+                    }
                 }
+                exam.questions.Add(question);
+
+                panel_questionList.BringToFront();
+
             }
-            exam.questions.Add(question);
+            catch (Exception ex)
+            {
+
+    
+            }
+   
 
 
 
@@ -335,6 +391,22 @@ namespace ExamSystem_Project.Forms
             }
 
 
+
+        }
+
+        private void button_ShowQuestions_Click(object sender, EventArgs e)
+        {
+            panel_questions.SendToBack();
+        }
+
+        private void button_AddQuestion_Click(object sender, EventArgs e)
+        {
+            panel_questions.BringToFront();
+            ClearAllControls();
+            question = new Question();
+
+
+          
 
         }
     }
